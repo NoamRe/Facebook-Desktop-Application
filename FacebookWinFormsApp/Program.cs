d2c358035b213116ab9692651bc22cb4f1957c3d
NoamRe <noamreches1@gmail.com>
2023-05-08 22:07:47 +0300

    .

---
+++ FacebookWinFormsApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BasicFacebookFeatures
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            UIManager.Instance.StartApplication();
+        }
+    }
+}
